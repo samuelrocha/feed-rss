864ee5f8ea271ae2a158efda0c58ecbb27f64618
Samuel Rocha <darocha.vasques@gmail.com>
2022-12-03 15:00:19 -0300

    completed login and registration system
    registration and login have been implemented and with no errors recognized so far, the front end will be performed last. @login_required decorator will be placed on all pages that require the active user

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,15 +1,17 @@
 from app import app, db, login_manager
 from flask import render_template, request, redirect
 from app.controllers.helpers import feed_rss, apology
-from app.models.forms import LoginForm
+from app.models.forms import LoginForm, RegisterForm
 from werkzeug.exceptions import HTTPException
+from werkzeug.security import check_password_hash, generate_password_hash
 from app.models.tables import User
 from flask_login import login_user, logout_user, login_required
+from sqlalchemy.exc import NoResultFound, IntegrityError
 
 
 @login_manager.user_loader
-def load_user(id):
-    return User.query.get(id)
+def load_user(username):
+    return User.query.get(username)
 
 
 @app.route('/')
@@ -19,28 +21,65 @@ def index():
     return render_template('index.html', feed=feed)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    logout_user()
+    msg = None
+    form = LoginForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+
+            try:
+                user = db.session.execute(
+                    db.select(User).filter_by(username=form.username.data)).one()
+
+                if check_password_hash(user[0].hash, form.password.data):
+                    login_user(user[0])
+                    return redirect('/')
+
+            except NoResultFound:
+                pass
+
+            msg = "incorrect username or password"
+
+    return render_template('login.html', form=form, msg=msg)
+
+
 @app.route('/logout')
-def test():
+def logout():
+
     logout_user()
     return redirect('/login')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    msg = None
+    logout_user()
+    form = RegisterForm()
+
     if request.method == 'POST':
         if form.validate_on_submit():
-            user = db.session.execute(
-                db.select(User).filter_by(username=form.username.data)).one()
 
-            if user[0] and user[0].hash == form.hash.data:
-                login_user(user[0])
+            hash = generate_password_hash(form.password.data)
+            user = User(form.username.data, form.name.data,
+                        form.email.data, hash)
+
+            try:
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
                 return redirect('/')
-            else:
-                return redirect('/login')
-        else:
-            return [form.username.errors, form.hash.errors]
-    return render_template('login.html', form=form)
+
+            except IntegrityError:
+                db.session.rollback()
+
+            msg = 'username or email already exists'
+
+    return render_template('register.html', form=form, msg=msg)
 
 
 @app.route('/add', methods=['GET', 'POST'])
--- app/models/forms.py
+++ app/models/forms.py
@@ -1,12 +1,22 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, BooleanField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    hash = PasswordField('hash', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me')
+    username = StringField('Username', [InputRequired(), Length(6, 21, "At least 6 characters, maximum 21")])
+    password = PasswordField('Password', [InputRequired(), Length(8, 21, "At least 8 characters, maximum 21")])
+    remember_me = BooleanField('Remember me')
+    recaptcha = RecaptchaField()
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', [InputRequired(), Length(6, 21, "At least 6 characters, maximum 21")])
+    name = StringField('Nickname', [InputRequired(), Length(3, 50, "At least 3 characters, maximum 50")])
+    email = StringField('E-mail',[InputRequired(), Email()])
+    password = PasswordField('Password', [InputRequired(), EqualTo('confirm', 'Password must be match'), Length(8, 21, "At least 8 characters, maximum 21")])
+    confirm = PasswordField('Repeat Password', [InputRequired(), Length(8, 21, "At least 8 characters, maximum 21")])
+    recaptcha = RecaptchaField()
+    
 
 class AddFeedForm(FlaskForm):
-    link = StringField('link', validators=[DataRequired()])
-    category = IntegerField('category', validators=[DataRequired()])
+    link = StringField('link', [InputRequired()])
+    category = IntegerField('category', [InputRequired()])
